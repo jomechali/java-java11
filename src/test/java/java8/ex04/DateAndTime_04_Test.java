1e779a8a254182e3928295530947de959cc2a3d2
formation <formation@DESKTOP-UNMM09V>
2022-06-28 14:55:21 +0200

    TP Java 17 DateAndTime

--- src/test/java/java8/ex04/DateAndTime_04_Test.java
+++ src/test/java/java8/ex04/DateAndTime_04_Test.java
@@ -2,7 +2,9 @@ package java8.ex04;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 
@@ -14,62 +16,63 @@ import static org.junit.Assert.*;
  */
 public class DateAndTime_04_Test {
 
-    @Test
-    public void test_localDateTime_of() {
+	@Test
+	public void test_localDateTime_of() {
 
-        // TODO créer un objet LocalDateTime à la date heure 12h00m00s 01/01/2018
-        LocalDateTime result = null;
+		// TODO créer un objet LocalDateTime à la date heure 12h00m00s 01/01/2018
+		LocalDateTime result = LocalDateTime.of(LocalDate.of(2018, 1, 1), LocalTime.of(12, 0, 0));
 
-        // TODO valoriser les différentes variables afin de rendre le test passant
-        int hour = 0;
-        int minutes = 0;
-        int second = 0;
-        int year = 0;
-        Month month = null;
-        int dayOfMonth = 0;
+		// TODO valoriser les différentes variables afin de rendre le test passant
+		int hour = result.getHour();
+		int minutes = result.getMinute();
+		int second = result.getSecond();
+		int year = result.getYear();
+		Month month = result.getMonth();
+		int dayOfMonth =result.getDayOfMonth();
 
-        assertThat(hour, is(12));
-        assertThat(minutes, is(0));
-        assertThat(second, is(0));
-        assertThat(year, is(2018));
-        assertThat(month, is(Month.JANUARY));
-        assertThat(dayOfMonth, is(1));
-    }
+		assertThat(hour, is(12));
+		assertThat(minutes, is(0));
+		assertThat(second, is(0));
+		assertThat(year, is(2018));
+		assertThat(month, is(Month.JANUARY));
+		assertThat(dayOfMonth, is(1));
+	}
 
-    @Test
-    public void test_localDateTime_parse() {
+	@Test
+	public void test_localDateTime_parse() {
 
-        // TODO créer un objet LocalDateTime à l'heure 2 mars 2009 à 09h30m00s à l'aide de la méthode parse
-        LocalDateTime result = null;
+		// TODO créer un objet LocalDateTime à l'heure 2 mars 2009 à 09h30m00s à l'aide
+		// de la méthode parse
+		LocalDateTime result = LocalDateTime.parse("2009-03-02T09:30:00");
+		
+		// TODO valoriser les différentes variables afin de rendre le test passant
+		int hour = result.getHour();
+		int minutes = result.getMinute();
+		int second = result.getSecond();
 
-        // TODO valoriser les différentes variables afin de rendre le test passant
-        int hour = 0;
-        int minutes = 0;
-        int second = 0;
+		int year = result.getYear();
+		Month month = result.getMonth();
+		int dayOfMonth = result.getDayOfMonth();
 
-        int year = 0;
-        Month month = null;
-        int dayOfMonth = 0;
+		assertThat(year, is(2009));
+		assertThat(month, is(Month.MARCH));
+		assertThat(dayOfMonth, is(2));
 
-        assertThat(year, is(2009));
-        assertThat(month, is(Month.MARCH));
-        assertThat(dayOfMonth, is(2));
+		assertThat(hour, is(9));
+		assertThat(minutes, is(30));
+		assertThat(second, is(0));
+	}
 
-        assertThat(hour, is(9));
-        assertThat(minutes, is(30));
-        assertThat(second, is(0));
-    }
+	@Test
+	public void test_localDateTime_format() {
 
-    @Test
-    public void test_localDateTime_format() {
+		// TODO créer un objet LocalDateTime le 27/11/2017 à 12h00m00s
+		// TODO utiliser la méthode of
+		LocalDateTime localDateTime = LocalDateTime.of(LocalDate.of(2017, 11, 27), LocalTime.of(12, 0, 0));
 
-        // TODO créer un objet LocalDateTime le 27/11/2017 à 12h00m00s
-        // TODO utiliser la méthode of
-        LocalDateTime localDateTime = null;
+		// TODO Formatter l'heure pour que le test soit passant
+		String result = localDateTime.format(DateTimeFormatter.ofPattern("yyyy - MM/dd hh:mm"));
 
-        // TODO Formatter l'heure pour que le test soit passant
-        String result = null;
-
-        assertThat(result, is("2017 - 11/27 12:00"));
-    }
+		assertThat(result, is("2017 - 11/27 12:00"));
+	}
 }
